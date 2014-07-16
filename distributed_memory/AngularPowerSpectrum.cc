56ed2d8d71875259e809788a437409b7804d48de
joyvgh <hillj@carleton.edu>
2014-07-16 15:12:54 -0500

    Reworked naming convention for temporary variables and provided further explanation and description of algorithms.

--- distributed_memory/AngularPowerSpectrum.cc
+++ distributed_memory/AngularPowerSpectrum.cc
@@ -177,9 +177,9 @@ void AngularPowerSpectrum::run() {
 
 
 void AngularPowerSpectrum::CreateOverdensity() {
-  DistMatrix<double, CIRC, CIRC> temp(*grid_);
-  temp.Attach(bins_, 1, *grid_, 0, 0, local_overdensity_, bins_);
-  overdensity_ = temp;
+  DistMatrix<double, CIRC, CIRC> temp_ovden(*grid_);
+  temp_ovden.Attach(bins_, 1, *grid_, 0, 0, local_overdensity_, bins_);
+  overdensity_ = temp_ovden;
 }
 
 void AngularPowerSpectrum::CalculateSignal() {
@@ -246,7 +246,8 @@ void AngularPowerSpectrum::CalculateSignal() {
       signal_[k].Attach(bins_, bins_, *grid_, 0, 0, local_signal[k].data(), 
           local_height_ );
 
-      //Sum matrix calculation: current used as a temporary vector with local_signal's data
+      //Sum matrix calculation: current used as a temporary vector 
+      //with local_signal's data; get's reset at the top of the loop
       current = local_signal[k];
       VectorTimesScalar(current, c_[k]);
       VectorPlusEqualsVector(local_sum, current);
@@ -263,12 +264,12 @@ void AngularPowerSpectrum::CalculateSignal() {
 
 void AngularPowerSpectrum::KLCompression() {
   is_compressed_ = true;
-  DistMatrix<double> temp(*grid_), temp2(*grid), B(*grid_), B_prime(*grid_), P(*grid_);
+  DistMatrix<double> temp_eig(*grid_), temp_transform(*grid), B(*grid_), B_prime(*grid_), P(*grid_);
   DistMatrix<double,VR,STAR> w(*grid_);
   double p, q;
   int cutoff;
   double *buffer;
-  Copy(sum_, temp);
+  Copy(sum_, temp_eig);
   Ones( P, bins_, bins_);
 
   //initializing p & q to create formula for values of the inverse Noise matrix
@@ -278,12 +279,12 @@ void AngularPowerSpectrum::KLCompression() {
   p = - kLargeNumber / ( inverse_density_ * (bins_ * kLargeNumber + inverse_density_) );
   q = 1.0 / inverse_density_;
 
-  //Storing Inverse Noise * Sum into temp
-  Symm(RIGHT, UPPER, p, sum_, P, q, temp);
+  //Storing Inverse Noise * Sum into temp_eig
+  Symm(RIGHT, UPPER, p, sum_, P, q, temp_eig);
 
-  //Elemental Eigensolver: solves temp, eigenvalues > w, eigenvectors > B, 
+  //Elemental Eigensolver: solves temp_eig, eigenvalues > w, eigenvectors > B, 
   //sorted in descending order
-  HermitianEig(UPPER, temp, w, B, DESCENDING);
+  HermitianEig(UPPER, temp_eig, w, B, DESCENDING);
 
   //Normalizing: B_{i,j} /= sqrt(N_{ij})
   buffer = B.Buffer();
@@ -310,19 +311,17 @@ void AngularPowerSpectrum::KLCompression() {
 
   //Create new Noise and Signal matrices
   Zeros(noise_, bins_, bins_);
-  Copy(B, temp2);
-  Gemm(TRANSPOSE, NORMAL, kLargeNumber, B_prime, P, inverse_density_, temp2);
-  Gemm(NORMAL, NORMAL, 1.0, temp2, B_prime, 0.0, noise_);
+  Copy(B, temp_transform);
+  Gemm(TRANSPOSE, NORMAL, kLargeNumber, B_prime, P, inverse_density_, temp_transform);
+  Gemm(NORMAL, NORMAL, 1.0, temp_transform, B_prime, 0.0, noise_);
   for (int i = 0; i < bands_; ++i){
-    Gemm(TRANSPOSE, NORMAL, 1.0, B_prime, signal_[i], 0.0, temp2);
+    Gemm(TRANSPOSE, NORMAL, 1.0, B_prime, signal_[i], 0.0, temp_transform);
     signal_[i] = DistMatrix<double>(*grid_);
     Zeros( signal_[i], bins_, bins_);
     local_signal[i]  = std::vector<double>();
-    Gemm(NORMAL, NORMAL, 1.0, temp2, B_prime, 0.0, signal_[i]);
+    Gemm(NORMAL, NORMAL, 1.0, temp_transform, B_prime, 0.0, signal_[i]);
   }
 }
-
-
 
 void AngularPowerSpectrum::CalculateDifference() {
   if (is_root_) std::cout << "Calculating Difference" << std::endl;
@@ -330,6 +329,7 @@ void AngularPowerSpectrum::CalculateDifference() {
   local_difference = std::vector<double>(local_height_ * local_width_, 0.0f);
   DistMatrix<double, STAR, STAR> all_overdensity = overdensity_;
 
+  //Difference = overdensity * overdensity transpose - Noise
   for( Int jLoc = 0; jLoc < local_width_; ++jLoc ) {
       const Int j = grid_col_ + jLoc*grid_width_;
       for( Int iLoc = 0; iLoc < local_height_; ++iLoc ) {
@@ -341,14 +341,15 @@ void AngularPowerSpectrum::CalculateDifference() {
 }
 
 void AngularPowerSpectrum::EstimateC() {
-  DistMatrix<double> P(*grid_), I(*grid_), temp1(*grid_), temp2(*grid_);
+  DistMatrix<double> P(*grid_), I(*grid_), temp_avg(*grid_);
   std::vector<DistMatrix<double>> A = std::vector<DistMatrix<double>>(bands_, DistMatrix<double>(*grid_));
   Matrix<double> fisher, average, W_prime;
+  DistMatrix<double>& covariance_inv;
 
   if (is_root_) std::cout << "Calculating Model Covariance Inverse" << std::endl;
 
+  //Must recalculate sum if compression has occurred
   if (iteration_ != 0 || is_compressed_) {
-    //Must recalculate sum
     if (is_root_) std::cout << "Calculating Sum" << std::endl;
     Zeros( sum_, bins_, bins_);
     for(int k = 0; k < bands_; ++k) {
@@ -365,16 +366,16 @@ void AngularPowerSpectrum::EstimateC() {
     Axpy(1.0, noise_, sum_);
   }
 
-  DistMatrix<double>& covariance_inv = sum_; //more apt variable name
-  SymmetricInverse(LOWER, covariance_inv); //this didn't work with UPPER
+  covariance_inv = sum_;
+  SymmetricInverse(LOWER, covariance_inv);
 
-  /* TODO(Alex): Could use Hemm for symetric multiplication */
-  if (is_root_) std::cout << "Fisher Matrix" << std::endl;
+  /* Fisher Matrix Calculation */
+  if (is_root_) std::cout << "Calculating Fisher Matrix" << std::endl;
   for (int k = 0; k < bands_; ++k) {
     Zeros(A[k], bins_, bins_);
+    /* TODO(Alex): Could use Hemm/Symm for symetric multiplication */
     Gemm(NORMAL, NORMAL, 1.0, covariance_inv, signal_[k], 0.0, A[k]);
   }
-
   Zeros(fisher, bands_, bands_);
   for (int k = 0; k < bands_; ++k) {
     for (int k_p = k; k_p < bands_; ++k_p) {
@@ -383,34 +384,41 @@ void AngularPowerSpectrum::EstimateC() {
       fisher.Set(k, k_p, result);
     }
   }
-  if (is_root_) Print(fisher, "fisher");
+  if (is_root_) Print(fisher, "Fisher");
 
+
+  /* Average Vector Calculation */
   if (is_root_) std::cout << "Calculating Average vector" << std::endl;
-  Zeros(temp1, bins_, bins_);
-  Zeros(temp2, bins_, bins_);
+  Zeros(temp_avg, bins_, bins_);
   Zeros(average, bands_, 1);
   for (int k = 0; k < bands_; ++k) {
-    Gemm(NORMAL, NORMAL, 1.0, A[k], covariance_inv, 0.0, temp1);
-    average.Set(k, 0, TraceMultiply(difference_, temp1));
+    Gemm(NORMAL, NORMAL, 1.0, A[k], covariance_inv, 0.0, temp_avg);
+    average.Set(k, 0, TraceMultiply(difference_, temp_avg));
   }
   
-
+  /* New Window Matrix and C Calculations */
   if (is_root_) {
     std::cout << "Calculating Window Matrix and New C" << std::endl;
     Matrix<double> fisher_inv_sqrt;
-    Matrix<double> Y, Y_inv, W, Z, temp, row, result;
+    Matrix<double> Y, Y_inv, W, W_prime, Z, temp_Z, row, result;
     std::vector<double> row_sum(bands_, 0.0);
+    //Initialize matrices to zero
+    Zeros(Y, bands_, bands_);
+    Zeros(Z, bands_, bands_);
+    Zeros(temp_Z, bands_, bands_);
+    Zeros(W_prime, bands_, bands_);
 
+    //Calculate fisher_inv_sqrt
     Copy(fisher, fisher_inv_sqrt);
     SymmetricInverse(LOWER, fisher_inv_sqrt);
     SquareRoot(fisher_inv_sqrt);
 
-    Zeros(Y, bands_, bands_);
-
+    //Calculate Y_inv
     Gemm(NORMAL, NORMAL, 1.0, fisher_inv_sqrt, fisher, 0.0, Y);
     Copy(Y, Y_inv);
     SymmetricInverse(LOWER, Y_inv);
 
+    //Calculate W
     Copy(Y, W);
     for (int j = 0; j < bands_; ++j) {
       for (int i = 0; i < bands_; ++i) {
@@ -421,14 +429,12 @@ void AngularPowerSpectrum::EstimateC() {
       }
     }
     
-    Zeros(Z, bands_, bands_);
-    Zeros(temp, bands_, bands_);
-    Zeros(W_prime, bands_, bands_);
-    Gemm(NORMAL, NORMAL, 1.0, W, Y_inv, 0.0, temp);
-
-    Gemm(NORMAL, NORMAL, 1.0, temp, fisher_inv_sqrt, 0.0, Z);
+    //Calculate Z & W_prime
+    Gemm(NORMAL, NORMAL, 1.0, W, Y_inv, 0.0, temp_Z);
+    Gemm(NORMAL, NORMAL, 1.0, temp_Z, fisher_inv_sqrt, 0.0, Z);
     Gemm(NORMAL, NORMAL, 1.0, Z, fisher, 0.0, W_prime);
     
+    //Build matrix of C_ls
     Zeros(result, 1, 1);
     for (int i = 0; i < bands_; ++i) {
       View(row, Z, i, 0, 1, bands_);
@@ -437,6 +443,8 @@ void AngularPowerSpectrum::EstimateC() {
     }
   }
   mpi::Broadcast(c_, bands_, 0, grid_->Comm());
+
+  //Save matrices
 # ifdef APS_OUTPUT_TEST
   SaveDistributedMatrix(std::string("iter_")+std::to_string(iteration_)+"_covariance_model" , covariance_inv);
   if (is_root_) {
