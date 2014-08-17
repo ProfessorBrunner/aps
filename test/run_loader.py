fb0268c4b0bcf1da8dcfb2e1778abafe6d2e380e
Alex Warren <amwarren@email.arizona.edu>
2014-08-16 22:55:52 -0700

    Starting paper and raw results. Also finishing runs to compare results on various node and process configurations

--- test/run_loader.py
+++ test/run_loader.py
@@ -23,7 +23,6 @@ def load_run(input_file):
             run.load_result()
         except Exception as e:
             print("Error loading {}".format(run))
-            print(e.message)
         else:
             loaded_runs.append(run)
     return loaded_runs
@@ -44,11 +43,12 @@ def node_compare_matcher(threads, mpi_nodes):
 
 def node_compare_lists(runs):
     times = []
+    kl_times = []
     total_memory = []
     per_node_memory = []
 
     for num_cores in [1, 2]:
-        for mpi_nodes in [1,2,3,4,5,6,7,8]:
+        for mpi_nodes in [1,2,3,4,5,6,7,8,9,10,11,12]:
             threads = 6 * num_cores
             matcher = node_compare_matcher(threads, mpi_nodes)
             samples = filter(matcher, runs)
@@ -56,60 +56,68 @@ def node_compare_lists(runs):
             #print [run.total_time for run in samples]
             if len(samples) == 0:
                 times.append(0.0)
+                kl_times.append(0.0)
                 total_memory.append(0.0)
                 continue
+            kl_times.append(min(
+                    run.time_results['KLCompression-eigensolve'][0] 
+                    for run in samples
+                    ))
             times.append(min(run.total_time for run in samples))
             mem = max(run.torque_mem for run in samples) / float(2**20)
             total_memory.append(mem)
+            if not times[-1] == 0.0:
+                print(kl_times[-1]/(times[-1]))
     print times
     print total_memory
 
     N = len(times)
     ind = np.arange(N)
-    width = .2
+    width = 1
 
     fig = plt.figure(figsize=(8,6))
     ax = plt.subplot()
     fig.subplots_adjust(bottom=0.25, left=.15)
-    memax = fig.add_axes(ax.get_position())
-    ax.set_ylim(0, 400)
-    memax.set_ylim(0, 2400)
-    memax.patch.set_visible(False)
-    memax.yaxis.set_label_position('right')
-    memax.yaxis.set_ticks_position('right')
-    memax.spines['bottom'].set_position(('outward', 35))
+    # memax = fig.add_axes(ax.get_position())
+    ax.set_ylim(0, 1600)
+    # memax.set_ylim(0, 2400)
+    # memax.patch.set_visible(False)
+    # memax.yaxis.set_label_position('right')
+    # memax.yaxis.set_ticks_position('right')
+    # memax.spines['bottom'].set_position(('outward', 35))
 
     ax.yaxis.grid(True)
     ax.set_ylabel('Time (seconds)', fontsize=16)
     ax.get_xaxis().set_visible(False)
-    memax.get_xaxis().set_visible(False)
-    memax.set_ylabel('Memory (megabytes)', fontsize=16)
-    memax.spines['bottom'].set_visible(False)
+    # memax.get_xaxis().set_visible(False)
+    # memax.set_ylabel('Memory (megabytes)', fontsize=16)
+    # memax.spines['bottom'].set_visible(False)
 
 
     rect_time = ax.bar(ind, times, width, color='r')
-    rect_mem = memax.bar(ind+width, total_memory, width, color='b')
+    rect_kl_time = ax.bar(ind, kl_times, width, color='g')
+    #rect_mem = memax.bar(ind+width, total_memory, width, color='b')
     #plt.axvline(x=3.8, ymin=-1, ymax = 1, linewidth=1, color='k')
 
-    ax.legend( (rect_time[0], rect_mem[0]), 
-            ('Runtime', 'Memory use'), 
-            loc=9, prop={'size':16}, bbox_to_anchor=[.6, 1.])
+    ax.legend( (rect_time[0], rect_kl_time[0]), 
+            ('Runtime', 'eigensolver time'), 
+            loc=9, prop={'size':16}, bbox_to_anchor=[.3, 1.])
 
     annotate_kwargs = {'xycoords':'axes fraction', 'textcoords':'offset points', 'va':'top', 'size':14}
 
 
     plt.annotate('Processes', (0,0), (-80, -15), **annotate_kwargs)
-    interval = 440.0/N
-    for i, val in zip(xrange(N), [1,2,2,4,2,4,4,8]):
-        plt.annotate(str(val), (0,0), (15+i*interval, -15), **annotate_kwargs)
+    interval = 420.0/N
+    for i, val in zip(xrange(N), [1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12]):
+        plt.annotate(str(val), (0,0), (5+i*interval, -15), **annotate_kwargs)
 
     plt.annotate('Cores', (0,0), (-80, -70), **annotate_kwargs)
-    interval = 440.0/(N/4.0)
+    interval = 420.0/(2)
     for i, val in zip(xrange(2), [1,2]):
         plt.annotate(str(val), (0,0), (97+i*interval, -70), **annotate_kwargs)
 
-    plt.show()
-    #fig.savefig("run_compare.png", bbox_inches='tight', dpi=180)
+    #plt.show()
+    fig.savefig("1_node_comp_processes_times.png", bbox_inches='tight', dpi=180)
 
 
 def plot_cl(runs):
@@ -132,12 +140,13 @@ def plot_cl(runs):
     plt.ylabel(r'$C_\ell$', fontsize=18)
     plt.legend(loc=0, numpoints=1)
     #plt.show()
-    fig.savefig("kl_cl.png", bbox_inches='tight', dpi=180)
+    #fig.savefig("kl_cl.png", bbox_inches='tight', dpi=180)
 
 
 
 if __name__ == "__main__":
     runs = load_run(INPUT_PICKLE)
-    #plot_cl(runs)
+    print(runs[0].time_results)
+    plot_cl(runs)
     node_compare_lists(runs)
 
